30293c30294f1a7b303c7c181d1eb1aee6e34dd9
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2025-04-03 08:57:42 +0200

    Binary_Search - master implemented the monobound quaternary search

--- monobound_quaternary_search/monobound_quaternary_search.cpp
+++ monobound_quaternary_search/monobound_quaternary_search.cpp
@@ -1,6 +1,15 @@
 #include "monobound_quaternary_search.h"
 
 
+/**
+ * Monobound quaternary search.
+ *
+ * @warning the array must be sorted.
+ *
+ * @param array the sorted array.
+ * @param key the target value to find in the array.
+ * @return the index of the target value, -1 if it is not present.
+ */
 int Monobound_Quaternary_Search::monobound_quaternary_search(const vector<int> &array, const int key) {
     const int array_size = static_cast<int>(array.size());
 
@@ -11,8 +20,43 @@ int Monobound_Quaternary_Search::monobound_quaternary_search(const vector<int> &
     int bottom = 0;
     int top = array_size;
     int middle = 0;
+    const int two_to_sixteen = static_cast<int>(pow(2, 16));
 
-    while (top > pow(2, 16)) {
-        middle = (top / 4);
+    while (top >= two_to_sixteen) {
+        middle = top / 4;
+        top -= middle * 3;
+
+        if (key < array[bottom + middle * 2]) {
+            if (key >= array[bottom + middle]) {
+                bottom += middle;
+            }
+        }
+        else {
+            bottom += middle * 2;
+
+            if (key >= array[bottom + middle]) {
+                bottom += middle;
+            }
+        }
     }
+
+    while (top > 3) {
+        middle = top / 2;
+
+        if (key >= array[bottom + middle]) {
+            bottom += middle;
+        }
+
+        top -= middle;
+    }
+
+    while (top > 0) {
+        top--;
+
+        if (key == array[bottom + top]) {
+            return bottom + top;
+        }
+    }
+
+    return -1;
 }
